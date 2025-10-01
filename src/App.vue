0e58c80cde31c45671db864334c7cc58d0bcaebe
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-10-01 04:12:41 +0300

    added base styles for app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>CRYPTO</h1>
+
+  <Input />
+  <div className="selectors">
+    <Selector />
+    <Selector />
+  </div>
+</template>
+
+<script>
+import Input from "./components/Input.vue";
+import Selector from "./components/Selector.vue";
+
+export default {
+  components: { Input, Selector },
+};
+</script>
+
+<style scoped>
+.selectors {
+  display: flex;
+  justify-content: space-around;
+  max-width: 700px;
+  width: 100%;
+  margin-inline: auto;
+}
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,22 @@
+<template>
+  <input type="number" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+input {
+  border-radius: 3px;
+  border: 0;
+  width: 500px;
+  position: relative;
+  top: -50px;
+  outline: none;
+  padding: 10px 15px;
+  background: #fafafa;
+  color: #333;
+  font-size: 1.2em;
+}
+</style>
